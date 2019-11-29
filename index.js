50dc13379750cd83cde0f078e1f507059b82f701
Leonard Ehrenfried <mail@leonard.io>
2019-11-29 11:14:27 +0100

    Read from ParkAPI

--- index.js
+++ index.js
@@ -1,69 +1,102 @@
-"use strict"
-const geojsonVt = require('geojson-vt');
-const vtPbf = require('vt-pbf');
-const request = require('requestretry');
-const zlib = require('zlib');
+"use strict";
+const geojsonVt = require("geojson-vt");
+const vtPbf = require("vt-pbf");
+const request = require("requestretry");
+const zlib = require("zlib");
+
+const url = process.env.PARK_API_URL || "https://api.parkendd.de/Ulm";
+const maxZoom = process.env.MAX_ZOOM || 20;
 
 const getTileIndex = (url, callback) => {
-  request({
-    url: url,
-    maxAttempts: 20,
-    retryDelay: 30000,
-    retryStrategy: (err, response) => (request.RetryStrategies.HTTPOrNetworkError(err, response) || (response && 202 === response.statusCode))
-  }, function (err, res, body){
-    if (err){
-      callback(err);
-      return;
+  request(
+    {
+      url: url,
+      maxAttempts: 20,
+      retryDelay: 30000,
+      retryStrategy: (err, response) =>
+        request.RetryStrategies.HTTPOrNetworkError(err, response) ||
+        (response && 202 === response.statusCode)
+    },
+    function(err, res, body) {
+      if (err) {
+        callback(err);
+        return;
+      }
+      callback(null, geojsonVt(parkApiToGeoJson(body), { maxZoom: maxZoom }));
     }
-    callback(null, geojsonVt(JSON.parse(body), {maxZoom: 20})); //TODO: this should be configurable)
-  })
-}
+  );
+};
 
-class GeoJSONSource {
-  constructor(uri, callback){
-    getTileIndex("https://s3.mobil-in-herrenberg.de/hb-parking/hb-parking.geojson", (err, tileIndex) => {
-      if (err){
+const parkApiToGeoJson = data => {
+  const json = JSON.parse(data);
+
+  const features = json.lots.map(lot => {
+    return {
+      type: "Feature",
+      geometry: {
+        type: "Point",
+        coordinates: [0, 0]
+      },
+      properties: {
+        name: "null island"
+      }
+    };
+  });
+
+  console.log(features);
+  return {
+    type: "FeatureCollection",
+    features: []
+  };
+};
+
+class ParkApiSource {
+  constructor(uri, callback) {
+    getTileIndex(url, (err, tileIndex) => {
+      if (err) {
         callback(err);
         return;
       }
       this.tileIndex = tileIndex;
       callback(null, this);
-    })
-  };
+    });
+  }
 
-  getTile(z, x, y, callback){
-    let tile = this.tileIndex.getTile(z, x, y)
+  getTile(z, x, y, callback) {
+    let tile = this.tileIndex.getTile(z, x, y);
 
-    if (tile === null){
-      tile = {features: []}
+    if (tile === null) {
+      tile = { features: [] };
     }
-    
-    const data = Buffer.from(vtPbf.fromGeojsonVt({'parking': tile}));
 
-    zlib.gzip(data, function (err, buffer) {
-      if (err){
+    const data = Buffer.from(vtPbf.fromGeojsonVt({ parking: tile }));
+
+    zlib.gzip(data, function(err, buffer) {
+      if (err) {
         callback(err);
         return;
       }
 
-      callback(null, buffer, {"content-encoding": "gzip"})
-    })
+      callback(null, buffer, { "content-encoding": "gzip" });
+    });
   }
 
-  getInfo(callback){
+  getInfo(callback) {
     callback(null, {
       format: "pbf",
       maxzoom: 20,
-      vector_layers: [{
-        description: "",
-        id: "parking"
-      }]
-    })
+      vector_layers: [
+        {
+          description: "Parking lots data retrieved from ParkApi",
+          id: "parking"
+        }
+      ]
+    });
   }
 }
 
-module.exports = GeoJSONSource
+module.exports = ParkApiSource;
 
-module.exports.registerProtocols = (tilelive) => {
-  tilelive.protocols['hbparking:'] = GeoJSONSource
-}
+module.exports.registerProtocols = tilelive => {
+  tilelive.protocols["hbparking:"] = ParkApiSource;
+};
